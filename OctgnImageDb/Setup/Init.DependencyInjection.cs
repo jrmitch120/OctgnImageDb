70ef7a1fbcca2b883c42a97425d6de3a9ed74f19
shobalk <shobalk@yahoo.com>
2014-10-10 23:00:34 -0500

    Remove unused namespaces.

--- OctgnImageDb/Setup/Init.DependencyInjection.cs
+++ OctgnImageDb/Setup/Init.DependencyInjection.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Ninject;
+﻿using Ninject;
 using OctgnImageDb.Imaging;
 using OctgnImageDb.Imaging.Doomtown;
 using OctgnImageDb.Imaging.Netrunner;
